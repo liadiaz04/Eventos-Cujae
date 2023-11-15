a98944ead1c959a5c8e971393af27cf3e311a4d5
liadiaz04 <liadiazzz04@gmail.com>
2023-11-15 12:25:17 -0500

    ww

--- src/inicio/Iniciadora.java
+++ src/inicio/Iniciadora.java
@@ -12,7 +12,7 @@ public class Iniciadora {
 				try {
 					Primera frame = new Primera();
 					frame.setVisible(true);
-					Sys
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
